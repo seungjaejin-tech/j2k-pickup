65f3ccc147e7e27579a3cda6114d1587f627fe91
seungjaejin-tech <seungjae.jin@sazo.shop>
2026-04-01 18:41:08 +0900

    Bump cache version to pickup-v3

--- sw.js
+++ sw.js
@@ -1,26 +1,4 @@
-const CACHE_NAME = "pickup-v2";
-const ASSETS = ["./index.html", "./manifest.json", "./icon.svg"];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (e) => {
-  if (e.request.method !== "GET") return;
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
-  );
-});const CACHE_NAME = "pickup-v1";
+const CACHE_NAME = "pickup-v3";
 const ASSETS = ["./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (e) => {
